21bdc13a1f891eeb0e68d105d87933b4436a982c
YvesCheung <975135274@qq.com>
2022-06-27 14:44:19 +0800

    init demo

--- app/src/main/java/io/github/yvescheung/adr/widget/textselection/MainActivity.kt
+++ app/src/main/java/io/github/yvescheung/adr/widget/textselection/MainActivity.kt
@@ -2,10 +2,21 @@ package io.github.yvescheung.adr.widget.textselection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import io.github.yvescheung.adr.widget.textselection.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.selectionBar.visibility = if (binding.editText.hasFocus()) VISIBLE else GONE
+        binding.editText.setOnFocusChangeListener { _, hasFocus ->
+            binding.selectionBar.visibility = if (hasFocus) VISIBLE else GONE
+        }
+        binding.selectionBar.setController(TextSelectionController(binding.editText))
     }
 }
